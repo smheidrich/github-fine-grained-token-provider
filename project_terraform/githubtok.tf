ab111684a4aeeea016ae2e36f5024165aac13e47
Shahriar Heidrich <smheidrich@weltenfunktion.de>
2023-12-24 18:21:50 +0100

    Add Terraform config for project itself

---
+++ project_terraform/githubtok.tf
@@ -0,0 +1,24 @@
+provider "githubtok" {
+}
+
+# TODO this has a bootstrapping problem in that the GitHub provider itself
+#   needs it, so when this gets (re)created, tf apply will fail due to the
+#   GitHub resource refresh not working with the unknown value. workaround
+#   is to only apply this resource and nothing else, which can be done via
+#   apply-github-tokens-only.sh
+resource githubtok_token "create_project_token" {
+  provider = githubtok
+  #name = "create-${var.project_name}"
+  name = "create-ghfgtp"
+  expires = "2024-12-10"
+  write_permissions = ["administration", "contents"]
+}
+
+resource githubtok_token "mirror_token" {
+  provider = githubtok
+  #name = "mirror-${var.project_name}"
+  name = "mirror-ghfgtp"
+  expires = "2024-12-10"
+  write_permissions = ["contents", "actions"]
+  select_repositories = [var.project_name]
+}
