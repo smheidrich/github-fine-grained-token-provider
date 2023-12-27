938024545668e6e1e2071d9f5772ee8fdae3dabd
Shahriar Heidrich <smheidrich@weltenfunktion.de>
2023-12-27 03:14:16 +0100

    Package with terradep launcher

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 name = github-fine-grained-token
+package_name = github_fine_grained_token_provider
+executable_name = github-fine-grained-token-provider
+main_module = github_fine_grained_token_provider
 version = 0.1.0
 os = linux
 arch = amd64
@@ -34,12 +37,33 @@ $(release_dir)/terraform-provider-$(name)_$(version)_manifest.json: \
 	mkdir -p $(release_dir)
 	cp $< $@
 
+## simple provider launcher script (no terradep):
+#$(release_dir)/terraform-provider-$(name)_$(version)_$(os)_$(arch).zip:
+	#mkdir -p $(release_dir)
+	#mkdir -p tmp
+	#sed -e 's/\.\././' terraform-plugin/terraform-provider-$(name)_v$(version) > tmp/terraform-provider-$(name)_v$(version)
+	#chmod +x tmp/terraform-provider-$(name)_v$(version)
+	#cp -r python-package tmp/
+	#cd tmp && zip -r ../$@ *
+
+## provider launcher script using terradep:
+#$(release_dir)/terraform-provider-$(name)_$(version)_$(os)_$(arch).zip:
+	#mkdir -p $(release_dir)
+	#rm -rf tmp
+	#mkdir -p tmp
+	#terradep-python generate --provider-name $(name) --provider-version $(version) --provider-python-package-name $(package_name) --provider-python-script-name $(executable_name) --provider-env-var-prefix GITHUB_FINE_GRAINED_TOKEN_PROVIDER tmp/
+	#( cd python-package && pip3 install --no-deps -t ../tmp/$(package_name) . )
+	#cd tmp && zip -r ../$@ *
+
+# provider launcher script using terradep, plus pre-installed relocatable venv:
 $(release_dir)/terraform-provider-$(name)_$(version)_$(os)_$(arch).zip:
 	mkdir -p $(release_dir)
+	rm -rf tmp
 	mkdir -p tmp
-	sed -e 's/\.\././' terraform-plugin/terraform-provider-$(name)_v$(version) > tmp/terraform-provider-$(name)_v$(version)
-	chmod +x tmp/terraform-provider-$(name)_v$(version)
-	cp -r python-package tmp/
+	terradep-python generate --provider-name $(name) --provider-version $(version) --provider-python-package-name $(package_name) --provider-python-main-module $(main_module) --provider-env-var-prefix GITHUB_FINE_GRAINED_TOKEN_PROVIDER tmp/
+	# TODO make exact Python version configurable
+	virtualenv -p python3 tmp/venv
+	( . tmp/venv/bin/activate && pip3 install ./python-package && virtualenv-make-relocatable tmp/venv )
 	cd tmp && zip -r ../$@ *
 
 clean:
--- python-package/github_fine_grained_token_provider.py
+++ python-package/github_fine_grained_token_provider.py
@@ -1,1 +1,296 @@
-provider.py
+import asyncio
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+from datetime import date, timedelta
+from os import environ
+from pathlib import Path
+from sys import stderr
+
+from github_fine_grained_token_client import (
+    AllRepositories,
+    AsyncClientSession,
+    FineGrainedTokenIndividualInfo,
+    GithubCredentials,
+    PermissionValue,
+    SelectRepositories,
+    TokenNameError,
+    TwoFactorOtpProvider,
+    async_client,
+)
+from github_fine_grained_token_client.common import Expired
+from github_fine_grained_token_client.permissions import permission_from_str
+from tfprovider.level2.attribute_path import ROOT
+from tfprovider.level2.diagnostics import Diagnostics
+from tfprovider.level2.wire_format import Unknown, UnrefinedUnknown
+from tfprovider.level2.wire_representation import (
+    DateAsStringWireRepresentation,
+    OptionalWireRepresentation,
+)
+from tfprovider.level3.statically_typed_schema import attribute, attributes_class
+from tfprovider.level4.async_provider_servicer import PlanResourceChangeResponse
+from tfprovider.level4.async_provider_servicer import Provider as BaseProvider
+from tfprovider.level4.async_provider_servicer import Resource as BaseResource
+
+
+class EnvTwoFactorOtpProvider(TwoFactorOtpProvider):
+    async def get_otp_for_user(self, username: str) -> str:
+        return environ["GITHUB_OTP"]
+
+
+@asynccontextmanager
+async def credentialed_client() -> AsyncIterator[AsyncClientSession]:
+    credentials = GithubCredentials(environ["GITHUB_USER"], environ["GITHUB_PASS"])
+    assert credentials.username and credentials.password
+    async with async_client(
+        credentials=credentials,
+        two_factor_otp_provider=EnvTwoFactorOtpProvider(),
+        persist_to=Path("~/.github-token-client/persist").expanduser(),
+    ) as session:
+        yield session
+
+
+@attributes_class()
+class ProviderConfig:
+    pass
+
+
+@attributes_class()
+class TokenResourceConfig:
+    id: str | None | Unknown = attribute(computed=True)
+    # TODO as I only found out now, anything that users can stick variables
+    #   into must be unknown-able, because the value given to planning when
+    #   there is a variable in the attr config will be a refined unknown with
+    #   that as the default (ext type 12); doing this for name now but also
+    #   needs to be done for the rest I think
+    name: str | Unknown = attribute(required=True)
+    expires: date | None = attribute(
+        optional=True,
+        computed=True,
+        default=None,
+        representation=OptionalWireRepresentation(DateAsStringWireRepresentation()),
+    )
+    value: str | Unknown | None = attribute(
+        default=None,
+        computed=True,
+        sensitive=True,
+        description="The (secret) token string",
+    )
+    # TODO the only way these are None is if a config is loaded in which they
+    #   are not set, but that should be taken care of by the
+    #   default_factory/default... so there should be special handling around
+    #   Terraform's usage of None values to get around this.
+    # TODO computed=True, this is only needed because we apply the
+    #   (non-None) default when planning; so compute=True should be set
+    #   automatically for anything with a non-None default.
+    # TODO see above re: unknown
+    select_repositories: set[str | Unknown] | None = attribute(
+        default_factory=set, optional=True, computed=True
+    )
+    read_permissions: set[str] | None = attribute(
+        default_factory=set, optional=True, computed=True
+    )
+    write_permissions: set[str] | None = attribute(
+        default_factory=set, optional=True, computed=True
+    )
+
+
+def token_resource_config_from_token_info(
+    token_info: FineGrainedTokenIndividualInfo,
+) -> TokenResourceConfig:
+    return TokenResourceConfig(
+        id=str(token_info.id),
+        name=token_info.name,
+        expires=token_info.expires.date()
+        if not isinstance(token_info.expires, Expired)
+        else date.today() - timedelta(days=1),
+        # TODO not currently returned by GHFGTC => rely on state (overwritten
+        #   by values from there at call site)
+        select_repositories=set(),
+        read_permissions=set(),
+        write_permissions=set(),
+    )
+
+
+class TokenResource(BaseResource[None, TokenResourceConfig]):
+    type_name = "githubtok_token"
+    config_type = TokenResourceConfig
+
+    schema_version = 1
+    block_version = 1
+
+    async def validate_resource_config(
+        self, config: TokenResourceConfig, diagnostics: Diagnostics
+    ) -> None:
+        print(f"vrc {config.name=}", file=stderr)
+
+    async def plan_resource_change(
+        self,
+        prior_state: TokenResourceConfig | None,
+        config: TokenResourceConfig,
+        proposed_new_state: TokenResourceConfig | None,
+        diagnostics: Diagnostics,
+    ) -> PlanResourceChangeResponse[TokenResourceConfig] | None:
+        if proposed_new_state is None:
+            return proposed_new_state
+        if proposed_new_state.expires is None:
+            proposed_new_state.expires = date.today() + timedelta(days=1)
+        if proposed_new_state.select_repositories is None:
+            proposed_new_state.select_repositories = set()
+        if proposed_new_state.read_permissions is None:
+            proposed_new_state.read_permissions = set()
+        if proposed_new_state.write_permissions is None:
+            proposed_new_state.write_permissions = set()
+        # TODO introduce convenience function for this:
+        if prior_state is not None and (
+            prior_state.name != proposed_new_state.name
+            or prior_state.expires != proposed_new_state.expires
+            or prior_state.select_repositories != proposed_new_state.select_repositories
+            or prior_state.read_permissions != proposed_new_state.read_permissions
+            or prior_state.write_permissions != proposed_new_state.write_permissions
+        ):
+            requires_replace = []
+            if prior_state.name != proposed_new_state.name:
+                requires_replace.append(ROOT.attribute_name("name"))
+            if prior_state.expires != proposed_new_state.expires:
+                requires_replace.append(ROOT.attribute_name("expires"))
+            if (
+                prior_state.select_repositories
+                != proposed_new_state.select_repositories
+            ):
+                requires_replace.append(ROOT.attribute_name("select_repositories"))
+            if prior_state.read_permissions != proposed_new_state.read_permissions:
+                requires_replace.append(ROOT.attribute_name("read_permissions"))
+            if prior_state.write_permissions != proposed_new_state.write_permissions:
+                requires_replace.append(ROOT.attribute_name("write_permissions"))
+            proposed_new_state.id = UnrefinedUnknown()
+            proposed_new_state.value = UnrefinedUnknown()
+        else:
+            requires_replace = None
+            if proposed_new_state.id is None:
+                proposed_new_state.id = UnrefinedUnknown()
+                proposed_new_state.value = UnrefinedUnknown()
+        return (
+            (proposed_new_state, requires_replace)
+            if requires_replace is not None
+            else proposed_new_state
+        )
+
+    async def apply_resource_change(
+        self,
+        prior_state: TokenResourceConfig | None,
+        config: TokenResourceConfig | None,
+        proposed_new_state: TokenResourceConfig | None,
+        diagnostics: Diagnostics,
+    ) -> TokenResourceConfig | None:
+        new_state = None
+        async with credentialed_client() as session:
+            if proposed_new_state is not None:
+                # proposed_new_state is based on config so this must hold:
+                assert config is not None
+                try:
+                    token_value = await session.create_token(
+                        proposed_new_state.name,
+                        expires=proposed_new_state.expires
+                        if proposed_new_state.expires is not None
+                        else timedelta(days=1),
+                        scope=SelectRepositories(list(rs))
+                        if (rs := proposed_new_state.select_repositories)
+                        else AllRepositories(),  # TODO allow publ repos
+                        permissions={
+                            **{
+                                permission_from_str(
+                                    permission_name
+                                ): PermissionValue.READ
+                                for permission_name in (
+                                    proposed_new_state.read_permissions or set()
+                                )
+                            },
+                            **{
+                                permission_from_str(
+                                    permission_name
+                                ): PermissionValue.WRITE
+                                for permission_name in (
+                                    proposed_new_state.write_permissions or set()
+                                )
+                            },
+                        },
+                    )
+                    diagnostics.add_warning(f"created token: {token_value}")
+                    token_info = await session.get_token_info_by_name(
+                        proposed_new_state.name
+                    )
+                except TokenNameError as e:
+                    diagnostics.add_error(f"not creating new token: {e}")
+                    return None
+                new_state = token_resource_config_from_token_info(token_info)
+                # TODO see comments on these in ^
+                new_state.select_repositories = config.select_repositories or set()
+                new_state.read_permissions = config.read_permissions or set()
+                new_state.write_permissions = config.write_permissions or set()
+                # not saved on GitHub so not part of token info either:
+                new_state.value = token_value
+            else:
+                if prior_state is None:
+                    return None
+                assert isinstance(prior_state.id, str), "bug"
+                await session.delete_token_by_id(int(prior_state.id))
+        return new_state
+
+    async def upgrade_resource_state(
+        self,
+        state: TokenResourceConfig,
+        version: int,
+        diagnostics: Diagnostics,
+    ) -> TokenResourceConfig:
+        return state
+
+    async def read_resource(
+        self, current_state: TokenResourceConfig, diagnostics: Diagnostics
+    ) -> TokenResourceConfig | None:
+        new_state = None
+        async with credentialed_client() as session:
+            try:
+                token_info = await session.get_token_info_by_name(current_state.name)
+                new_state = token_resource_config_from_token_info(token_info)
+                # TODO see comments on these in ^
+                new_state.select_repositories = (
+                    current_state.select_repositories or set()
+                )
+                new_state.read_permissions = current_state.read_permissions or set()
+                new_state.write_permissions = current_state.write_permissions or set()
+                # the token value itself is not saved on GitHub's end so we
+                # *have to* take it from the current state:
+                new_state.value = current_state.value
+            except KeyError:
+                diagnostics.add_warning("token not found, but thats ok")
+        return new_state
+
+    async def import_resource(
+        self, id: str, diagnostics: Diagnostics
+    ) -> TokenResourceConfig:
+        async with credentialed_client() as session:
+            token_info = await session.get_token_info_by_id(int(id))
+        return TokenResourceConfig(id=id, name=token_info.name)
+
+
+class Provider(BaseProvider[None, ProviderConfig]):
+    provider_state = None
+    resource_factories = [TokenResource]
+    config_type = ProviderConfig
+
+    schema_version = 1
+    block_version = 1
+
+    async def validate_provider_config(
+        self, config: ProviderConfig, diagnostics: Diagnostics
+    ) -> None:
+        print("vpc", file=stderr)
+
+
+def main() -> None:
+    s = Provider()
+    asyncio.run(s.run())
+
+
+if __name__ == "__main__":
+    main()
